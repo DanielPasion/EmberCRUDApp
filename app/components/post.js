f3fc06aa1fd65a73a80353d330c0b9e042d91662
DanielPasion <dnpaxion@gmail.com>
2024-04-09 14:49:29 -0700

    Implemented Delete Feature

--- app/components/post.js
+++ app/components/post.js
@@ -1,4 +1,12 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class PostComponent extends Component {
+   @action async getID() {
+      let {post} = this.args;
+      let id = post["id"]
+      let response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id, {method: "DELETE"})
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+   }
 }
